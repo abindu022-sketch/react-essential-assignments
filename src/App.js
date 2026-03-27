a4e8966b54033ccf62ba23ba84d0775a4308344d
AdusumilliHimabindu <142237656+AdusumilliHimabindu@users.noreply.github.com>
2026-03-27 12:36:13 +0530

    CHANGE IN APP.JS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-//1. Import statemetnts (if needed).
 import './App.css';
 import PortfolioCard from './components/PortfolioCard';
 
-// 2. Component Function (always starts with capital letter!)
 function App(){
-
-  //3. Javascripty logic goes here
-
-  //4. Return JSX (the UI)
+  
   return (
     <div className="App">
       <PortfolioCard />
@@ -15,5 +10,4 @@ function App(){
   );
 }
 
-//5. exort so other files can use it
 export  default App;
